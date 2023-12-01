775cb773ed546f77defa85c51d8a3a08509caa55
Monchekin <barthelsonlina@gmail.com>
2023-12-01 20:17:06 +0100

    G delen funkar med kod av VG delen

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-let express = require("express");
-let app = express();
-app.listen(3000);
-console.log("Servern körs på port 3000");
-
+const express = require("express");
+const app = express();
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 const mysql = require("mysql");
+
+app.listen(3000);
+console.log("Server is running on port 3000");
+
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "jensen2023",
+  database: "api-bas",
   multipleStatements: true,
 });
 
@@ -18,46 +21,57 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/homepage.html");
 });
 
-app.post("/users", function (req, res) {
+app.post("/users", async function (req, res) {
+  const fields = ["firstname", "lastname", "username", "password", "email"];
+
   if (!req.body.username) {
     res.status(400).send("username required!");
     return;
   }
-  let fields = ["username", "password", "name", "email"];
-  for (let key in req.body) {
-    if (!fields.includes(key)) {
-      res.status(400).send("Unknown field: " + key);
-      return;
+  console.log("user:", req.body);
+
+  const missingFields = fields
+    .filter((field) => !req.body[field])
+    .join(", ");
+
+  if (missingFields) {
+    res.status(422).send(`Invalid data: ${missingFields} are required.`);
+  } else if (isValidUserData(req.body)) {
+    try {
+      const result = await sqlUsers(req, res);
+      res.json(result);
+    } catch (err) {
+      handleError(err, res);
     }
+  } else {
+    res.status(422).send(`Invalid data: ${fields} are required.`);
   }
-  let sql = `INSERT INTO users (username, password, name, email)
-    VALUES ('${req.body.username}', 
-    '${req.body.password}',
-    '${req.body.name}',
-    '${req.body.email}');
-    SELECT LAST_INSERT_ID();`;
-
-  console.log(sql);
-
-  con.query(sql, function (err, result, fields) {
-    if (err) throw err;
-
-    console.log(result);
-    let output = {
-      id: result[0].insertId,
-      username: req.body.username,
-      password: req.body.password,
-      name: req.body.name,
-      email: req.body.email,
-    };
-    res.send(output);
-  });
 });
 
-// LÄSER FRÅN DATABASEN
-const COLUMNS = ["id", "username", "password", "name", "email"];
+const COLUMNS = ["id", "firstname", "lastname", "username", "password", "email"];
 
 app.get("/users", function (req, res) {
+  let authHeader = req.headers["authorization"];
+  if (authHeader === undefined) {
+    res.sendStatus(400);
+    return;
+  }
+  let token = authHeader.slice(7);
+
+  console.log(token);
+
+  let decoded;
+  try {
+    decoded = jwt.verify(token, "EnHemlighetSomIngenKanGissaXyz123%&/");
+  } catch (err) {
+    console.log(err);
+    res.status(401).send("Invalid auth token");
+    return;
+  }
+
+  console.log(decoded);
+  console.log(`Tjena ${decoded.firstname}! Din mailadress är ${decoded.email}.`);
+
   let sql = "SELECT * FROM users";
   let condition = createCondition(req.query);
   console.log(sql + condition);
@@ -80,16 +94,137 @@ app.get("/users/:id", function (req, res) {
   });
 });
 
-function createCondition(query) {
+app.put("/users/:id", function (req, res) {
+  if (!(req.body && req.body.firstname && req.body.email && req.body.password)) {
+    res.sendStatus(400);
+    return;
+  }
+  let sql = `UPDATE users 
+        SET firstname = '${req.body.firstname}', 
+        lastname = '${req.body.lastname}', 
+        username = '${req.body.username}', 
+        email = '${req.body.email}', 
+        password = '${req.body.password}'
+        WHERE id = ${req.params.id}`;
+
+  con.query(sql, function (err, result, fields) {
+    if (err) {
+      throw err;
+    } else {
+      res.status(200).send('Allt funkar som det ska');
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  if (!(req.body && req.body.username && req.body.password)) {
+    res.sendStatus(400);
+    return;
+  }
+
+  let sql = `SELECT * FROM users WHERE username='${req.body.username}'`;
+
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    if (result.length == 0) {
+      res.sendStatus(401);
+      return;
+    }
+
+    let passwordHash = hash(req.body.password);
+
+    if (result[0].password == passwordHash) {
+      res.send({
+        subObject: createSubObject(result)
+      });
+
+      let payload = {
+        subObject: createSubObject(result)
+      };
+      let token = jwt.sign(payload, "OMGvadskamanhaförhemligtlösenord-1234?");
+      res.json(token);
+
+    } else {
+      res.sendStatus(401);
+    }
+  });
+});
+
+// Functions
+
+const createSubObject = (result) => ({
+  sub: result[0].username,
+  firstname: result[0].firstname,
+  lastname: result[0].lastname,
+  email: result[0].email
+});
+
+const createCondition = (query) => {
   let output = " WHERE ";
   for (let key in query) {
     if (COLUMNS.includes(key)) {
       output += `${key}="${query[key]}" OR `;
     }
   }
-  if (output.length == 7) {
-    return "";
-  } else {
-    return output.substring(0, output.length - 4);
-  }
-}
+  return output.length === 7 ? "" : output.substring(0, output.length - 4);
+};
+
+const sqlUsers = async (req, res) => {
+  return new Promise((resolve, reject) => {
+    const sql = `INSERT INTO users (firstname, lastname, username, password, email)
+      VALUES (
+        '${req.body.firstname}',
+        '${req.body.lastname}',
+        '${req.body.username}', 
+        '${hash(req.body.password)}',
+        '${req.body.email}');
+      SELECT LAST_INSERT_ID();`;
+
+    con.query(sql, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        const output = {
+          id: result.insertId,
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email,
+        };
+        resolve(output);
+      }
+    });
+  });
+};
+
+
+const isValidUserData = (body) => {
+  return body && body.firstname && body.lastname && body.username && body.password &&  body.email;
+
+};
+
+const hash = (data) => {
+  const hash = crypto.createHash("sha256");
+  hash.update(data);
+  return hash.digest("hex");
+};
+
+const handleError = (err, res) => {
+  console.error(err);
+  res.status(500).send("Internal Server Error");
+};
+
+
+// const result = await con.query(sql);
+// const output = {
+//   id: result[0].insertId,
+//   firstname: req.body.firstname,
+//   lastname: req.body.lastname,
+//   username: req.body.username,
+//   password: req.body.password,
+//   email: req.body.email,
+// };
+
+// res.json(output);
+
